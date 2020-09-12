9dd6c170aac9a171bbd07a89acbfc0f1471e2950
Deecee <savateev@i-teco.ru>
2020-09-12 23:39:01 +0300

    Initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# D666ce.github.io
---
+++ precache-manifest.2d6fa0591e7b7a8c45426699fad71d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8cecb1920f5dfd819453d2fabebd4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d63a620083903f82c5b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6a17858ec67804c089eb",
+    "url": "/static/js/2.36352534.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.36352534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d63a620083903f82c5b",
+    "url": "/static/js/main.bd61a6e1.chunk.js"
+  },
+  {
+    "revision": "f70b9d2919b2dbcbe4ac",
+    "url": "/static/js/runtime-main.c51b08c6.js"
+  }
+]);
